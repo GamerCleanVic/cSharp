dc5a97c8b6e717a72d384d5cdb9f25ead2dab082
GamerCleanVic <jonassd2000@gmail.com>
2022-08-31 22:16:32 -0500

    inicial

---
+++ proj1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace proj1{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Diogo Costa Curso C#");
+            //Mantém na tela
+            Console.ReadKey();
+        }
+    }
+}
+
